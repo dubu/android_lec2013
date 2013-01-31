581d3b3401092dfc12a09043b07af498874b86c4
dubu <kozazz@hanmail.net>
2013-01-31 17:57:53 +0900

    homework chap4 aa

---
+++ src/com/kingkongdubu/homework/chap04/CustomComponent.java
@@ -0,0 +1,23 @@
+package com.kingkongdubu.homework.chap04;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: Administrator
+ * Date: 13. 1. 31
+ * Time: 오후 5:54
+ * To change this template use File | Settings | File Templates.
+ */
+public class CustomComponent extends Activity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+
+                    // aaa
+
+        super.onCreate(savedInstanceState);
+    }
+}
