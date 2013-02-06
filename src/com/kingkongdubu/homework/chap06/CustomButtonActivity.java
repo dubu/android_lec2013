96ce1164c5c3fb1cf3e7553b9caf9c79fdba4130
dubu <kozazz@hanmail.net>
2013-02-06 19:25:11 +0900

    homework chap6

---
+++ src/com/kingkongdubu/homework/chap06/CustomButtonActivity.java
@@ -0,0 +1,21 @@
+package com.kingkongdubu.homework.chap06;
+
+import android.app.Activity;
+import android.os.Bundle;
+import com.kingkongdubu.R;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: Administrator
+ * Date: 13. 2. 6
+ * Time: 오후 6:50
+ * To change this template use File | Settings | File Templates.
+ */
+public class CustomButtonActivity extends Activity{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.homework_chap06);
+    }
+}
