53d707594d72939d82d624cc32e4d362280da995
dubu <kozazz@hanmail.net>
2013-02-06 10:45:25 +0900

    lec chap6

---
+++ src/com/kingkongdubu/lecture/chap06/Chap6Activity.java
@@ -0,0 +1,21 @@
+package com.kingkongdubu.lecture.chap06;
+
+import android.app.Activity;
+import android.os.Bundle;
+import com.kingkongdubu.R;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: Administrator
+ * Date: 13. 2. 6
+ * Time: 오전 9:16
+ * To change this template use File | Settings | File Templates.
+ */
+public class Chap6Activity  extends Activity{
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.class_chap06);
+    }
+}
