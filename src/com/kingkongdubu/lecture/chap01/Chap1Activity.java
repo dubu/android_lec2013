894adc3649ed79c9f6ad0a2e2b19bcbba2caecf8
dubu <kozazz@hanmail.net>
2013-02-10 15:08:10 +0900

    add chap1 lecture

---
+++ src/com/kingkongdubu/lecture/chap01/Chap1Activity.java
@@ -0,0 +1,12 @@
+package com.kingkongdubu.lecture.chap01;
+
+/**
+ * User: Administrator
+ * Date: 13. 2. 10
+ * Time: 오후 3:06
+ */
+public class Chap1Activity {
+
+    // intro android mobile develop
+    // setting develop env.
+}
