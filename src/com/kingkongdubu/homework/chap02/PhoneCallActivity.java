5e9e2a410855120f05ce404663c6ffe5f089dc66
dubu <kozazz@hanmail.net>
2013-02-25 17:06:30 +0900

    chap2 decode image size

--- src/com/kingkongdubu/homework/chap02/PhoneCallActivity.java
+++ src/com/kingkongdubu/homework/chap02/PhoneCallActivity.java
@@ -4,6 +4,7 @@ import android.app.Activity;
 import android.content.ActivityNotFoundException;
 import android.content.Intent;
 import android.database.Cursor;
+import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.net.Uri;
 import android.os.Bundle;
@@ -14,6 +15,9 @@ import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
 import com.kingkongdubu.R;
+
+import java.io.File;
+import java.io.FileNotFoundException;
 
 /**
  * User: Administrator
@@ -76,11 +80,46 @@ public class PhoneCallActivity extends Activity {
             cursor.close();
 
             ImageView imageView = (ImageView) findViewById(R.id.imageView);
-            imageView.setImageBitmap(BitmapFactory.decodeFile(picturePath));
+
+            // case01. occur big size image , memory problem
+            //imageView.setImageBitmap(BitmapFactory.decodeFile(picturePath));
+
+            // case02. resize big image
+            Uri uri = Uri.fromFile(new File(picturePath));
+            try {
+                imageView.setImageBitmap(decodeUri(uri));
+            } catch (FileNotFoundException e) {
+                e.printStackTrace();
+            }
 
         }
 
 
+    }
+
+
+    private Bitmap decodeUri(Uri selectedImage) throws FileNotFoundException {
+        BitmapFactory.Options o = new BitmapFactory.Options();
+        o.inJustDecodeBounds = true;
+        BitmapFactory.decodeStream(getContentResolver().openInputStream(selectedImage), null, o);
+
+        final int REQUIRED_SIZE = 140;
+
+        int width_tmp = o.outWidth, height_tmp = o.outHeight;
+        int scale = 1;
+        while(true) {
+            if(width_tmp / 2 < REQUIRED_SIZE || height_tmp / 2 < REQUIRED_SIZE) {
+                break;
+            }
+
+            width_tmp /= 2;
+            height_tmp /= 2;
+            scale *= 2;
+        }
+
+        BitmapFactory.Options o2 = new BitmapFactory.Options();
+        o2.inSampleSize = scale;
+        return BitmapFactory.decodeStream(getContentResolver().openInputStream(selectedImage), null, o2);
     }
 
 
